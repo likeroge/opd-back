7aa5fb7d7aacf4d0d1634f3fa021a0e1d7ba9a7d
likeroge <likeroge@gmail.com>
2024-03-04 00:27:17 +0400

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,16 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.setGlobalPrefix('api');
   const config = new DocumentBuilder()
     .setTitle('OPD API')
     .setDescription('OPD API description')
     .setVersion('1.0')
     .addTag('opd')
+    .setBasePath('api')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('doc', app, document);
-  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
